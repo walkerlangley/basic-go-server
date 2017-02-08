4d516efcc1b5011b7f19e341b8f27e00a49a9d06
Walker Langley <walkerlangley@gmail.com>
2017-02-07 21:22:37 -0500

    Moving from vanilla sql to sqlx

--- server.go
+++ server.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -12,20 +12,22 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
 )
 
 type Book struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	Author      string `json:"author"`
-	Description string `json:"description,omitempty"`
-	ImgUrl      string `json:"imageUrl,omitempty"`
-	Notes       string `json:"notes,omitempty"`
-	YearWritten string `json:"yearWritten,omitempty"`
+	ID          string  `json:"id" db:"id"`
+	Title       string  `json:"title" db:"title"`
+	Author      string  `json:"author" db:"author"`
+	Description *string `json:"description,omitempty" db:"description,omitempty"`
+	ImageUrl    *string `json:"imageUrl,omitempty" db:"imageUrl,omitempty"`
+	Notes       *string `json:"notes,omitempty" db:"notes,omitempty"`
+	YearWritten *string `json:"yearWritten,omitempty" db:"yearWritten,omitempty"`
+	Read        bool    `json:"read" db:"read"`
 }
 
 var port = "4001"
-var db *sql.DB
+var db *sqlx.DB
 var err error
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
@@ -59,50 +61,177 @@ func login(w http.ResponseWriter, r *http.Request) {
 
 }
 
+//func GetBookByTitle(w http.ResponseWriter, r *http.Request) {
+//vars := mux.Vars(r)
+//title := vars["title"]
+
+//stmt, err := db.Prepare("SELECT * FROM books WHERE title = ?")
+//if err != nil {
+//fmt.Println("Error preparing query statement: ", err)
+//}
+//var book Book
+//var ID string
+//var Title string
+//var Author string
+//var Description sql.NullString
+//var ImageUrl sql.NullString
+//var Notes sql.NullString
+//var YearWritten sql.NullString
+//var Read bool
+//err = stmt.QueryRow(title).Scan(&ID, &Title, &Author, &Description, &ImageUrl, &Notes, &YearWritten, &Read)
+//switch {
+//case err == sql.ErrNoRows:
+//log.Printf("No book with that title")
+//case err != nil:
+//log.Fatal(err)
+//default:
+//book.ID = ID
+//book.Title = Title
+//book.Author = Author
+//book.Description = Description.String
+//book.ImageUrl = ImageUrl.String
+//book.Notes = Notes.String
+//book.YearWritten = YearWritten.String
+//book.Read = Read
+//}
+//json.NewEncoder(w).Encode(book)
+//}
+
+//func GetBooksByAuthor(w http.ResponseWriter, r *http.Request) {
+//vars := mux.Vars(r)
+//author := vars["author"]
+
+//stmt, err := db.Prepare("SELECT * FROM books WHERE author = ?")
+//if err != nil {
+//fmt.Println("Error preparing query statement: ", err)
+//}
+//var book Book
+//var books []Book
+
+//rows, err := stmt.Query(author)
+//if err != nil {
+//log.Println("Error Getting Rows", err)
+//}
+
+//defer rows.Close()
+
+//for rows.Next() {
+//var ID string
+//var Title string
+//var Author string
+//var Description sql.NullString
+//var ImageUrl sql.NullString
+//var Notes sql.NullString
+//var YearWritten sql.NullString
+//var Read bool
+
+//if err := rows.Scan(&ID, &Title, &Author, &Description, &ImageUrl, &Notes, &YearWritten, &Read); err != nil {
+//log.Println("Error in Rows: ", err)
+//}
+
+//book.ID = ID
+//book.Title = Title
+//book.Author = Author
+//book.Description = Description.String
+//book.ImageUrl = ImageUrl.String
+//book.Notes = Notes.String
+//book.YearWritten = YearWritten.String
+//book.Read = Read
+//books = append(books, book)
+//}
+//err = rows.Err()
+//if err != nil {
+//log.Fatal(err)
+//}
+//json.NewEncoder(w).Encode(book)
+//}
+
 func GetBooks(w http.ResponseWriter, r *http.Request) {
 
-	var book Book
 	var books []Book
-	rows, err := db.Query("SELECT * FROM books")
+	//stmt, err := db.Prepare("SELECT * FROM books")
+	//if err != nil {
+	//fmt.Println("Error preparing the query statement: ", err)
+	//}
+	err = db.Select(&books, "SELECT * FROM books")
+
+	//rows, err := stmt.Queryx()
 	if err != nil {
 		log.Println("Error Getting Rows", err)
 	}
 
-	defer rows.Close()
+	//defer rows.Close()
 
-	for rows.Next() {
-		var ID string
-		var Title string
-		var Author string
-		var Description sql.NullString
-		var ImageUrl sql.NullString
-		var Notes sql.NullString
-		var YearWritten sql.NullString
+	//for rows.Next() {
+	//// This is another way of scanning into the struct, but lose types since everything is an interface
+	//col, _ := rows.Columns()
+	//numCols := len(col)
 
-		if err := rows.Scan(&ID, &Title, &Author, &Description, &ImageUrl, &Notes, &YearWritten); err != nil {
-			log.Println("Error in Rows: ", err)
-		}
-		book.ID = ID
-		book.Title = Title
-		book.Author = Author
-		book.Description = Description.String
-		book.ImgUrl = ImageUrl.String
-		book.Notes = Notes.String
-		book.YearWritten = YearWritten.String
+	//rowStruct := make([]interface{}, numCols)
+	//if err := rows.Scan(&rowStruct...); err != nil {
+	//log.Println("Error in Rows: ", err)
+	//}
 
-		books = append(books, book)
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+	//var ID string
+	//var Title string
+	//var Author string
+	//var Description sql.NullString // Could also use a pointer to a string instead of sql.NullString since pointers can be null
+	//var ImageUrl sql.NullString
+	//var Notes sql.NullString
+	//var YearWritten sql.NullString
+	//var Read bool
+
+	//if err := rows.Scan(&ID, &Title, &Author, &Description, &ImageUrl, &Notes, &YearWritten, &Read); err != nil {
+	//log.Println("Error in Rows: ", err)
+	//}
+
+	//book.ID = ID
+	//book.Title = Title
+	//book.Author = Author
+	//book.Description = Description.String
+	//book.ImageUrl = ImageUrl.String
+	//book.Notes = Notes.String
+	//book.YearWritten = YearWritten.String
+	//book.Read = Read
+	//books = append(books, book)
+	//}
+	//err = rows.Err()
+	//if err != nil {
+	//log.Fatal(err)
+	//}
 
 	json.NewEncoder(w).Encode(books)
 }
 
+func AddBook(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Error reading body: ", err)
+	}
+
+	var book Book
+	err = json.Unmarshal(body, &book)
+	if err != nil {
+		log.Println("Error Unmarshallin body: ", err)
+	}
+
+	stmt, err := db.Prepare("INSERT INTO `books`(`title`, `author`, `description`, `imageUrl`, `notes`, `yearWritten`, `read`) VALUES(?,?,?,?,?,?,?);")
+	if err != nil {
+		fmt.Println("Error preparing the query statement: ", err)
+	}
+	result, err := stmt.Exec(book.Title, book.Author, book.Description, book.ImageUrl, book.Notes, book.YearWritten, book.Read)
+	if err != nil {
+		log.Println("Error Creating Record", err)
+	}
+
+	log.Println("Query Result: ", result.LastInsertId)
+
+	log.Println("BODY: ", book)
+}
+
 func main() {
 
-	db, err = sql.Open("mysql", "root:@/library")
+	db, err = sqlx.Open("mysql", "root:@/library")
 	if err != nil {
 		log.Println("Error connecting to db: ", err.Error())
 	}
@@ -128,7 +257,9 @@ func main() {
 	api.HandleFunc("/", sayHelloName)
 	api.HandleFunc("/login", login)
 	api.HandleFunc("/books", GetBooks).Methods("GET")
-
+	//api.HandleFunc("/book/title/{title}", GetBookByTitle).Methods("GET")
+	//api.HandleFunc("/books/author/{author}", GetBooksByAuthor).Methods("GET")
+	api.HandleFunc("/book", AddBook).Methods("POST")
 	server := &http.Server{
 		Handler: router,
 		Addr:    "127.0.0.1:" + port,
